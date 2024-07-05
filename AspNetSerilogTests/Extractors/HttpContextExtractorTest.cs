fab355f7d88632ad3bcc30b961490942476d05ff
henrique.knabach <henrique.knabach@pagar.me>
2024-07-05 13:24:56 -0300

    :white_check_mark: Add unit tests

--- AspNetSerilogTests/Extractors/HttpContextExtractorTest.cs
+++ AspNetSerilogTests/Extractors/HttpContextExtractorTest.cs
@@ -1,16 +1,20 @@
 using AspNetSerilog.Extractors;
+using AspNetSerilogTests.Mocks;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace AspNetSerilogTests.Extractors
 {
     public class HttpContextExtractorTest
     {
-        private HttpContext _httpContext;
+        private readonly HttpContext _httpContext;
 
         public HttpContextExtractorTest()
         {
-            _httpContext = new DefaultHttpContext();
+            _httpContext = HttpContextMock.DefaultHttpContext();
         }
 
         [Fact]
@@ -126,6 +130,108 @@ namespace AspNetSerilogTests.Extractors
             var actual = HttpContextExtractor.GetFullUrl(_httpContext, queryBlackList, httpContextBlackList);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetContentAsObjectByContentTypeJson_WithFilledBlacklist_ReturnsMaskedBody()
+        {
+            // Arrange
+            const string EXPECTED_VALUE = "{\"Test\":\"1\",\"Card\":{\"Number\":\"******\",\"Password\":\"******\",\"Email\":\"email@test.com\",\"Card\":{\"Number\":\"******\",\"Password\":\"******\"},\"Teste\":{\"Card\":{\"Number\":\"******\",\"Password\":\"******\",\"Email\":\"email@test.com\"}}}}";
+
+            string[] queryBlackList = { "*card.number", "*password" };
+
+            // Act
+            var value = _httpContext.GetRequestBody(queryBlackList);
+
+            // Assert
+            Assert.Equal(EXPECTED_VALUE, JsonConvert.SerializeObject(value));
+        }
+
+        [Fact]
+        public void GetContentAsObjectByContentTypeJson_WithEmptyBlacklist_ReturnsBodyWithoutMask()
+        {
+            // Arrange
+            const string EXPECTED_VALUE = "{\"Test\":\"1\",\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\",\"Email\":\"email@test.com\",\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\"},\"Teste\":{\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\",\"Email\":\"email@test.com\"}}}}";
+
+            string[] queryBlackList = { };
+
+            // Act
+            var value = _httpContext.GetRequestBody(queryBlackList);
+
+            // Assert
+            Assert.Equal(EXPECTED_VALUE, JsonConvert.SerializeObject(value));
+        }
+
+        [Fact]
+        public void GetContentAsObjectByContentTypeJson_WithBlacklistAndPartialBlacklist_ReturnsBodyMaskedAndPartialMasked()
+        {
+            // Arrange
+            const string EXPECTED_VALUE = "{\"Test\":\"1\",\"Card\":{\"Number\":\"462294*****9865\",\"Password\":\"******\",\"Email\":\"email@test.com\",\"Card\":{\"Number\":\"462294*****9865\",\"Password\":\"******\"},\"Teste\":{\"Card\":{\"Number\":\"462294*****9865\",\"Password\":\"******\",\"Email\":\"email@test.com\"}}}}";
+
+            var blacklistPartialMock = BlacklistPartialMock.DefaultBlackListPartial;
+
+            string[] queryBlackList = { "*card.number", "*password" };
+
+            // Act
+            var value = _httpContext.GetRequestBody(queryBlackList, blacklistPartialMock);
+
+            // Assert
+            Assert.Equal(EXPECTED_VALUE, JsonConvert.SerializeObject(value));
+        }
+
+        [Fact]
+        public void GetContentAsObjectByContentTypeJson_WithEmptyBlacklistAndFilledPartialBlacklist_ReturnsBodyWithoutMask()
+        {
+            // Arrange
+            const string EXPECTED_VALUE = "{\"Test\":\"1\",\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\",\"Email\":\"email@test.com\",\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\"},\"Teste\":{\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\",\"Email\":\"email@test.com\"}}}}";
+
+            var blacklistPartialMock = BlacklistPartialMock.DefaultBlackListPartial;
+
+            string[] queryBlackList = { };
+
+            // Act
+            var value = _httpContext.GetRequestBody(queryBlackList, blacklistPartialMock);
+
+            // Assert
+            Assert.Equal(EXPECTED_VALUE, JsonConvert.SerializeObject(value));
+        }
+
+        [Fact]
+        public void GetContentAsObjectByContentTypeJson_WithFilledBlacklistAndEmptyPartialBlacklist_ReturnsBodyWithTotalMask()
+        {
+            // Arrange
+            const string EXPECTED_VALUE = "{\"Test\":\"1\",\"Card\":{\"Number\":\"******\",\"Password\":\"******\",\"Email\":\"email@test.com\",\"Card\":{\"Number\":\"******\",\"Password\":\"******\"},\"Teste\":{\"Card\":{\"Number\":\"******\",\"Password\":\"******\",\"Email\":\"email@test.com\"}}}}";
+
+            var blacklistPartialMock = new Dictionary<string, Func<string, string>>(StringComparer.OrdinalIgnoreCase) { };
+
+            string[] queryBlackList = { "*card.number", "*password" };
+
+            // Act
+            var value = _httpContext.GetRequestBody(queryBlackList, blacklistPartialMock);
+
+            // Assert
+            Assert.Equal(EXPECTED_VALUE, JsonConvert.SerializeObject(value));
+        }
+
+        [Fact]
+        public void GetContentAsObjectByContentTypeJson_WithInvalidFunctionInDictionaryOfPartialBlacklist_ShouldAbortJsonMaskingAndReturnValueUnmodified()
+        {
+            // Arrange
+            const string EXPECTED_VALUE = "{\"Test\":\"1\",\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\",\"Email\":\"email@test.com\",\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\"},\"Teste\":{\"Card\":{\"Number\":\"4622943127049865\",\"Password\":\"somepass#here2\",\"Email\":\"email@test.com\"}}}}";
+            const int INDEX_OUT_OF_RANGE = 100;
+
+            var blacklistPartialWithInvalidFuncMock = new Dictionary<string, Func<string, string>>(StringComparer.OrdinalIgnoreCase)
+            {
+                {"*card.number", value => value[INDEX_OUT_OF_RANGE..] }
+            };
+
+            string[] queryBlackList = { "*card.number", "*password" };
+
+            // Act
+            var valueUnmodified = _httpContext.GetRequestBody(queryBlackList, blacklistPartialWithInvalidFuncMock);
+
+            // assert
+            Assert.Equal(EXPECTED_VALUE, valueUnmodified);
         }
     }
 }
